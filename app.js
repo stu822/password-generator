64e92b8e01af8aa7c12635fa4515f4d54a0446ae
Stuart Abel <stu822@gmail.com>
2023-07-17 14:20:30 -0500

    create random character functionality and arrays for characters

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// prettier-ignore
+const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_'];
+// prettier-ignore
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// prettier-ignore
+const alphabet = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+
 const form = document.querySelector("#form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -13,3 +23,8 @@ form.addEventListener("submit", (e) => {
 
   console.log(numberChoice, selectedOptions);
 });
+
+function randomCharacter(arr) {
+  let idx = Math.floor(Math.random() * arr.length);
+  return arr[idx];
+}
