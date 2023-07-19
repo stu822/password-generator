0896ae7bce36d3109df07d9a206e9e6319806c81
Stuart Abel <stu822@gmail.com>
2023-07-19 13:38:38 -0500

    add basic generator functionality

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // prettier-ignore
-const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_'];
+const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '-', '_'];
 // prettier-ignore
 const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 // prettier-ignore
@@ -9,9 +9,11 @@ const alphabet = [
 ];
 
 const form = document.querySelector("#form");
+const password = document.querySelector("#password");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const numberChoice = document.querySelector("#number-choice").value;
+  let generatedPassword;
   const selectedOptions = [];
   const options = document.getElementsByName("option");
 
@@ -21,10 +23,38 @@ form.addEventListener("submit", (e) => {
     }
   }
 
-  console.log(numberChoice, selectedOptions);
+  for (let i = 0; i <= numberChoice; i++) {
+    const chacterType = randomSelection(selectedOptions);
+
+    if (i === 0) {
+      if (chacterType === "lowercase") {
+        generatedPassword = randomSelection(alphabet);
+      } else if (chacterType === "uppercase") {
+        const uppercase = randomSelection(alphabet);
+        generatedPassword = randomSelection(uppercase.toUpperCase());
+      } else if (chacterType === "symbol") {
+        generatedPassword = randomSelection(specialCharacters);
+      } else {
+        generatedPassword = randomSelection(numbers);
+      }
+    } else {
+      if (chacterType === "lowercase") {
+        generatedPassword += randomSelection(alphabet);
+      } else if (chacterType === "uppercase") {
+        const uppercase = randomSelection(alphabet);
+        generatedPassword += randomSelection(uppercase.toUpperCase());
+      } else if (chacterType === "symbol") {
+        generatedPassword += randomSelection(specialCharacters);
+      } else {
+        generatedPassword += randomSelection(numbers);
+      }
+    }
+  }
+
+  password.textContent = generatedPassword;
 });
 
-function randomCharacter(arr) {
+function randomSelection(arr) {
   let idx = Math.floor(Math.random() * arr.length);
   return arr[idx];
 }
